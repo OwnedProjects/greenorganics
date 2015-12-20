33761d0baac4cceb257f7ebe8104ebd71cf80479
wasimakram-mulla <wasim3ace@gmail.com>
2015-12-20 19:22:21 +0530

    Dashboard Updated with Pending Stocks

--- js/Controllers/homepagecontroller.js
+++ js/Controllers/homepagecontroller.js
@@ -25,6 +25,35 @@ greenorganics.controller("DashboardController", function($scope, $http, $route){
 				$(".fullData").hide();
 			}			
 		});
+		
+		$(".loadDataOrder").show();
+		$(".noDataOrder").hide();
+		$(".fullDataOrder").hide();
+		$http({
+			method: 'GET',
+			url: 'php/master.php?action=allPendingOrders',
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).
+		error(function(data, status, headers, config) {
+			alert('Service Error');
+		}).
+		then(function(result){
+			if(result.data.status==true){
+				$(".loadDataOrder").hide();
+				$(".noDataOrder").hide();
+				$(".fullDataOrder").show();
+				$scope.salesData=result.data.salesData;				
+			}
+			else{
+				$(".loadDataOrder").hide();
+				$(".noDataOrder").show();
+				$(".fullDataOrder").hide();
+			}			
+		});
 	};
 	$scope.fetchStockDetails();
+	
+	$scope.reload = function(){
+		$route.reload();
+	};
 });
--- php/master.php
+++ php/master.php
@@ -1096,4 +1096,28 @@ $action=$_GET['action'];
 		}
 		echo json_encode($obj);
 	}
+	
+	if($action=='allPendingOrders'){
+		$selAccClients="SELECT * FROM `sales_register`,`client_master` WHERE sales_register.sale_status='open' and sales_register.client_id=client_master.client_id order by `order_date`";
+		$resAccClients=mysql_query($selAccClients);
+		$count = mysql_num_rows($resAccClients);
+		if($count>0){
+			$cnt=0;
+			while($row = mysql_fetch_array( $resAccClients )) {
+				$tmpRes[$cnt]->sales_id=$row['sales_id'];
+				$tmpRes[$cnt]->order_no=$row['order_no'];
+				$tmpRes[$cnt]->order_date=$row['order_date'];
+				$tmpRes[$cnt]->sale_status=$row['sale_status'];
+				$tmpRes[$cnt]->quantity=$row['quantity'];
+				$tmpRes[$cnt]->client_name=$row['client_name'];
+				$cnt++;
+			}
+			$obj->status=true;
+			$obj->salesData=$tmpRes;
+		}
+		else{
+			$obj->status=false;
+		}
+		echo json_encode($obj);
+	}
 ?>
